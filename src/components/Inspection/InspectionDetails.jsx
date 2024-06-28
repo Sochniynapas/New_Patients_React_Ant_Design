45b25436871f16ff2675e93dfa168ef9bb64d233
Sochniynapas <66013035+Sochniynapas@users.noreply.github.com>
2024-06-29 02:28:56 +0700

    Сделал основные элементы деталей осмотра без комментариев

--- src/components/Inspection/InspectionDetails.jsx
+++ src/components/Inspection/InspectionDetails.jsx
@@ -3,10 +3,13 @@ import { useGetInspectionDetailsQuery } from "../../api/inspectionsApi"
 import { useEffect } from "react"
 import { useNavigate, useParams } from "react-router-dom"
 import {
+  formatedConclusion,
   formatedDate,
   formatedDateWithTime,
+  formatedDiagType,
   formatedGender,
 } from "../../helpers/formatters"
+import TextArea from "antd/es/input/TextArea"
 
 const InspectionDetails = () => {
   const { Title, Text } = Typography
@@ -43,7 +46,9 @@ const InspectionDetails = () => {
                 </Title>
                 <Button type="primary">Редактировать осмотр</Button>
               </Row>
-              <Title level={4}>Пациент: {details.patient.name}</Title>
+              <Title style={{ marginTop: 0 }} level={4}>
+                Пациент: {details.patient.name}
+              </Title>
               <Col span={24}>
                 <Text>Пол: {formatedGender(details.patient.gender)}</Text>
               </Col>
@@ -59,16 +64,71 @@ const InspectionDetails = () => {
               </Col>
             </Card>
             <Card style={{ marginTop: "20px" }} className="form">
-              <Title level={2}>Анамнез заболевания</Title>
-              <Form.Item name={"anamnesis"}>
-                <TextArea></TextArea>
-              </Form.Item>
+              <Title style={{ marginTop: 0 }} level={2}>
+                Жалобы
+              </Title>
+              <Text>{details.complaints}</Text>
             </Card>
             <Card style={{ marginTop: "20px" }} className="form">
-              <Title level={2}>Анамнез заболевания</Title>
-              <Form.Item name={"anamnesis"}>
-                <TextArea></TextArea>
-              </Form.Item>
+              <Title style={{ marginTop: 0 }} level={2}>
+                Анамнез заболевания
+              </Title>
+              <Text>{details.anamnesis}</Text>
+            </Card>
+            {details.diagnoses && (
+              <Card style={{ marginTop: "20px" }} className="form">
+                <Title style={{ marginTop: 0 }} level={2}>
+                  Диагнозы
+                </Title>
+                {details.diagnoses.map((elem, index) => (
+                  <Col
+                    style={{ paddingBottom: 20 }}
+                    span={24}
+                    key={elem.icdDiagnosisId}
+                  >
+                    <Row>
+                      <Title style={{ marginTop: 0 }} level={5}>
+                        ({elem.code}) {elem.name}
+                      </Title>
+                    </Row>
+                    <Row>
+                      <Text type="secondary">
+                        Тип в осмотре: {formatedDiagType(elem.type)}
+                      </Text>
+                    </Row>
+                    <Row>
+                      <Text type="secondary">
+                        Расшифровка: {elem.description}
+                      </Text>
+                    </Row>
+                  </Col>
+                ))}
+              </Card>
+            )}
+            <Card style={{ marginTop: "20px" }} className="form">
+              <Title style={{ marginTop: 0 }} level={2}>
+                Рекомендации по лечению
+              </Title>
+              <Text>{details.treatment}</Text>
+            </Card>
+            <Card style={{ marginTop: "20px" }} className="form">
+              <Title style={{ marginTop: 0 }} level={2}>
+                Заключение
+              </Title>
+              <Title level={5}>{formatedConclusion(details.conclusion)}</Title>
+              {details.conclusion !== "Recovery" && (
+                <>
+                  {details.conclusion === "Death" ? (
+                    <Text>
+                      Дата смерти: {formatedDateWithTime(details.deathDate)}
+                    </Text>
+                  ) : (
+                    <Text>
+                      Дата следующего визита: {formatedDateWithTime(details.nextVisitDate)}
+                    </Text>
+                  )}
+                </>
+              )}
             </Card>
           </>
         )}
